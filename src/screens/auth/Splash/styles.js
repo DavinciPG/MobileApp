913a08e98de7c4624de52d4ccc1d084f5551f6a5
Erkz <Erkiparna7@gmail.com>
2024-02-08 09:26:47 +0200

    Splash
    Started splash screen
    Switched from App.tsx to App.js to avoid typescript

---
+++ src/screens/auth/Splash/styles.js
@@ -0,0 +1,25 @@
+import { StyleSheet } from 'react-native'
+
+export const styles = StyleSheet.create({
+    image: {
+        width: '100%',
+        height: 200,
+        paddingLeft: 9,
+        paddingRight: 9,
+        paddingTop: 144
+    },
+    container: {
+        
+    },
+    title: {
+        fontSize: 40,
+        fontWeight: "bold",
+        paddingLeft: 64,
+        paddingRight: 64,
+        textAlign: 'center'
+    },
+    innerTitle: {
+        color: '#FCA34D',
+        textDecorationLine: 'underline'
+    }
+});
